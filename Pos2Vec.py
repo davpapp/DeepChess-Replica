11a94850e4fa770518ab77b24b54ef973ed523f2
dpapp <david.papp@students.olin.edu>
2019-12-08 11:27:57 -0500

    dataset is working

--- Pos2Vec.py
+++ Pos2Vec.py
@@ -1,6 +1,7 @@
 import chess.pgn
 import chess
 import pickle
+import numpy as np
 
 import torch
 import torchvision
@@ -33,6 +34,31 @@ class GamesDataset(Dataset):
         return sample
 
 
+class BitstringToTensor(object):
+    """ Converts a bitstring in sample to Tensors. """
+
+    def __call__(self, sample):
+        board, outcome = sample['board'], sample['outcome']
+
+        # Convert the bitstring to a numpy array:
+        # https://stackoverflow.com/questions/29091869/convert-bitstring-string-of-1-and-0s-to-numpy-array
+        board_array = np.fromstring(board,'u1') - ord('0')
+        board_tensor = torch.from_numpy(board_array)
+
+        outcome_code = 0
+        if (outcome == '1-0'):
+            outcome_code = 0
+        elif (outcome == '1/2-1/2'):
+            outcome_code = 1
+        else:
+            outcome_code = 2
+        outcome_tensor = torch.tensor(outcome_code)
+        print(outcome, ' -> ', outcome_code, " -> ", outcome_tensor)
+
+        return {'board': board_tensor, 'outcome': outcome_tensor}
+
+
+
 
 """
 class Autoencoder(nn.Module):
@@ -63,11 +89,21 @@ class Autoencoder(nn.Module):
 
 with open('parsed_games/2015-05.bare.[6004].parsed_flattened.pickle', 'rb') as handle:
     games_data = pickle.load(handle)
-    print(games_data[:5])
-    print(len(games_data))
+    #print(games_data[:5])
+    #print(len(games_data))
 
     games = [game[0] for game in games_data]
-    labels = [game[1] for game in games_data]
-    print(games[:5])
-    print(labels[:5])
-    games_dataset = GamesDataset(games, labels)
+    outcomes = [game[1] for game in games_data]
+    #print(games[:2])
+    #print(labels[:60])
+
+    games_dataset = GamesDataset(boards=games,
+                                labels=outcomes,
+                                transform=transforms.Compose([BitstringToTensor()]))
+
+    for i in range(len(games_dataset)):
+        sample = games_dataset[i]
+        print(i, sample['board'].size(), sample['outcome'].size())
+
+        if i == 100:
+            break
